51cc5037feecd2a36dd555511e99b7acf0881086
Victor DEFONTAINE <vikthor.d@gmail.com>
2017-07-20 15:36:55 +0200

    [CONFIG] Add natives for osx & windows

--- project.clj
+++ project.clj
@@ -7,5 +7,7 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [org.slf4j/slf4j-log4j12 "1.7.12"]
                  [org.lwjgl/lwjgl "3.0.0b"]
-                 [org.lwjgl/lwjgl-platform "3.0.0b" :classifier "natives-linux"]]
+                 [org.lwjgl/lwjgl-platform "3.0.0b" :classifier "natives-linux"]
+                 [org.lwjgl/lwjgl-platform "3.0.0b" :classifier "natives-windows"]
+                 [org.lwjgl/lwjgl-platform "3.0.0b" :classifier "natives-osx"]]
   :main test-lwjgl.core) 
