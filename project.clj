b4769c5d8713801c5412d39358e8a71b70219c38
Victor DEFONTAINE <vikthor.d@gmail.com>
2017-10-16 19:40:50 +0200

    config for tests

--- project.clj
+++ project.clj
@@ -2,14 +2,15 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [net.mikera/vectorz-clj "0.47.0"]
-		 [thi.ng/math "0.2.1"]
+		             [thi.ng/math "0.2.1"]
                  [org.slf4j/slf4j-log4j12 "1.7.12"]
                  [org.lwjgl/lwjgl "3.0.0b"]
                  [org.lwjgl/lwjgl-platform "3.0.0b" :classifier "natives-linux"]
                  [org.lwjgl/lwjgl-platform "3.0.0b" :classifier "natives-windows"]
                  [org.lwjgl/lwjgl-platform "3.0.0b" :classifier "natives-osx"]]
-  :main test-lwjgl.core) 
+  :main test-lwjgl.core
+  :repl-options {:init-ns test-lwjgl.core}) 
