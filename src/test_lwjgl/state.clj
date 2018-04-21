12297367584a3626204cd46349683cbc27f92970
victor Defontaine <vikthor.d@gmail.com>
2018-04-21 13:28:26 +0200

    Fix previous commit

--- src/test_lwjgl/state.clj
+++ src/test_lwjgl/state.clj
@@ -1,20 +1,20 @@
 (ns test-lwjgl.state)
 
 (defn new-motion
-  ([]      (init-motions 0.5))
+  ([]      (new-motion 0.5))
   ([speed] {:directions {:forward false :backward false :left false :right false}
             :speed      speed}))
 
-(defn new-mouse {:mod       :camera ;; possible values: [:camera :normal]
-            :sensivity 0.05
-            :position  {:x 0.0 :y 0.0}})
+(defn new-mouse [] {:mod       :camera ;; possible values: [:camera :normal]
+                    :sensivity 0.05
+                    :position  {:x 0.0 :y 0.0}})
 
-(defn new-camera {:mod       :free
-             :position  [0.0 10.0 10.0]
-             :front     [0.0 0.0 -1.0]
-             :up        [0.0 1.0 0.0]
-             :angles    {:yaw -90.0 :pitch 0.0 :roll 0.0}
-             :motion    (new-motion)})
+(defn new-camera [] {:mod       :free
+                     :position  [0.0 10.0 10.0]
+                     :front     [0.0 0.0 -1.0]
+                     :up        [0.0 1.0 0.0]
+                     :angles    {:yaw -90.0 :pitch 0.0 :roll 0.0}
+                     :motion    (new-motion)})
 
 (def state (atom {:camera (new-camera)
                   :mouse  (new-mouse)
